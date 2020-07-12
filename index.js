e4747d15868db1d4523347af7f9850408476397e
felipe.bertanha <felipe.bertanha@capitaletecnologia.com.br>
2020-07-12 19:40:58 -0300

    aula 3.1 - added mysql connection

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const customExpress = require('./config/customExpress')
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress()
+conexao.connect(null, (erro) => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log('MySQL conectado com sucesso!');
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+        const app = customExpress();
+
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+});
+
