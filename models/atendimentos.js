8077732f372a730002329d50e08862ef2d50991a
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-07-18 18:32:10 -0300

    aula 4.3- added request validation

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,6 +7,30 @@ class Atendimentos {
     adiciona(atendimento, res) {
 
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+
+        const dataEhValida = moment(atendimento.data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Deve deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+            return;
+        }
         const atendimentoDatado = {...atendimento, dataCriacao};
 
         const sql = ` INSERT INTO atendimentos SET ? `;
